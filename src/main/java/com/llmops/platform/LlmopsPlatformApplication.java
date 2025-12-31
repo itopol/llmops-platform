ee9fd6231a5a5498e43c9faf3cd35eeee67a12d0
Igor Topol <itopol@medline.com>
2025-12-31 11:42:42 -0600

    Initial Spring Boot skeleton (Java 21)
    # Conflicts:
    #	.gitignore

---
+++ src/main/java/com/llmops/platform/LlmopsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.llmops.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LlmopsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LlmopsPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/llmops/platform/LlmopsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llmops.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LlmopsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
